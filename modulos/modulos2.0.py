5fbb1191d67511ac52bc433e29e19ce1138fe5b6
Joselin Zarate <zaratejoselin594@gmail.com>
2023-07-10 11:58:58 -0300

    python

--- modulos/modulos2.0.py
+++ modulos/modulos2.0.py
@@ -1,4 +1,6 @@
 # si el modulo estuviera dentro de una carpeta en la misma ruta
 
 import sys
+
+sys.path.append("C:\Users\karol\OneDrive\Escritorio\codigo\estudio\py>")
 print(sys.builtin_module_names)
